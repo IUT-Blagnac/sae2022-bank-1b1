36a29f0b21f95b9f2452daf7cca4a6efadc0b616
matthieurobert <matthieurobert82@gmail.com>
2022-05-31 15:52:33 +0200

    Création de la fonctionnalité "créer un compte"

--- DAILY_BANK_FX/src/application/control/ComptesManagement.java
+++ DAILY_BANK_FX/src/application/control/ComptesManagement.java
@@ -10,6 +10,7 @@ import application.tools.StageManagement;
 import application.view.ComptesManagementController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -17,10 +18,7 @@ import javafx.stage.Stage;
 import model.data.Client;
 import model.data.CompteCourant;
 import model.orm.AccessCompteCourant;
-import model.orm.exception.ApplicationException;
-import model.orm.exception.DatabaseConnexionException;
-import model.orm.exception.Order;
-import model.orm.exception.Table;
+import model.orm.exception.*;
 
 public class ComptesManagement {
 
@@ -88,26 +86,22 @@ public class ComptesManagement {
 		compte = cep.doCompteEditorDialog(this.clientDesComptes, null, EditionMode.CREATION);
 		if (compte != null) {
 			try {
-				// Temporaire jusqu'à implémentation
+				AccessCompteCourant acc = new AccessCompteCourant();
+
+				acc.insertCompteCourant(compte);
+			} catch (ManagementRuleViolation me) {
+				AlertUtilities.showAlert(this.primaryStage, "Erreur", "Montant initial trop faible",
+						"Le montant initial doit être supérieur à 50", AlertType.ERROR);
 				compte = null;
-				AlertUtilities.showAlert(this.primaryStage, "En cours de développement", "Non implémenté",
-						"Enregistrement réel en BDD du compe non effectué\nEn cours de développement", AlertType.ERROR);
-
-				// TODO : enregistrement du nouveau compte en BDD (la BDD donne de nouvel id
-				// dans "compte")
-
-				// if JAMAIS vrai
-				// existe pour compiler les catchs dessous
-				if (Math.random() < -1) {
-					throw new ApplicationException(Table.CompteCourant, Order.INSERT, "todo : test exceptions", null);
-				}
 			} catch (DatabaseConnexionException e) {
 				ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, e);
 				ed.doExceptionDialog();
 				this.primaryStage.close();
+				compte = null;
 			} catch (ApplicationException ae) {
 				ExceptionDialog ed = new ExceptionDialog(this.primaryStage, this.dbs, ae);
 				ed.doExceptionDialog();
+				compte = null;
 			}
 		}
 		return compte;
